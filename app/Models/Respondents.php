8120c069c63e35408efbb609ba0aea198337bbe7
AldiMardiansah <aldimardiansah9@gmail.com>
2024-07-11 16:43:43 +0700

    Add some coloumn for respondents and user models

--- app/Models/Respondents.php
+++ app/Models/Respondents.php
@@ -10,10 +10,19 @@ class Respondents extends Model
     use HasFactory;
 
     protected $fillable = [
+        'email',
         'name',
-        'gender',
         'address',
-        'status'
+        'gender',
+        'age',
+        'status',
+        'doses',
+        'vaccine',
+        'effects',
+        'medical_history',
+        'importance',
+        'info_sufficiency',
+        'service_rate',
     ];
 
     public static function getValidGenders()
@@ -22,5 +31,67 @@ class Respondents extends Model
             'Laki-laki' => 'Laki-laki',
             'Perempuan' => 'Perempuan',
         ];
+    }
+
+    public static function getValidStatuses()
+    {
+        return [
+            'sudah vaksin' => 'Sudah divaksin',
+            'belum vaksin' => 'Belum divaksin',
+        ];
+    }
+
+    public static function getValidDoses() //jumlah vaksinasi
+    {
+        return ['1', '2', '3', '4', '5'];
+    }
+
+    public static function getValidVaccines() //jenis yang divaksin
+    {
+        return [
+            'Sinovac',
+            'AstraZeneca',
+            'Moderna',
+            'Pfizer-BioNTech',
+            'Sinopharm',
+        ];
+    }
+
+    public static function getValidImportance() //skala pentingnya vaksin
+    {
+        return [
+        '1' => 'Tidak penting',
+        '2' => '',
+        '3' => '',
+        '4' => '',
+        '5' => 'Sangat penting'
+    ];
+    }
+
+    public static function getValidInfo() //informasi kelengkapan vaksin
+    {
+        return [
+        '1' => 'Tidak lengkap',
+        '2' => '',
+        '3' => '',
+        '4' => '',
+        '5' => 'Sangat lengkap'
+    ];
+    }
+
+    public static function getValidRatings() //rate pelayanan yg diberikan
+    {
+        return [
+        '1' => 'Sangat buruk',
+        '2' => '',
+        '3' => '',
+        '4' => '',
+        '5' => 'Sangat baik'
+    ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'email', 'email');
     }
 }
